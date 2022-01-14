aea9ce8806c1e74c0ad7ec883beac14cfecb2369
Nhlamulo Socha <nchauke@csir.co.za>
2022-01-14 14:24:33 +0200

    init

---
+++ src/test/java/com/tdd/nhlamulo/SpringBootTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.nhlamulo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
